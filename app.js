97714f4287cb662ce7e3e4bb4805402543f9176f
carlosjc31 <carlosjc31@hotmail.com>
2025-01-20 16:04:40 -0300

    3º commit criando as funções para buscar amigos na lista

--- app.js
+++ app.js
@@ -1,13 +1,58 @@
+
 let amigos = [];
 
-function adicionarAmigo() {
-    let nomeAmigo = document.querySelector('input').value;
 
-    if(nomeAmigo){
+function adicionarAmigo() {
+
+    const input = document.getElementById('amigo');
+    const nome = input.value.trim();
+
+
+    if (nome === '') {
         alert('Por favor, insira um nome.');
         return;
     }
-    listaDeAmigos.push(nomeAmigo);
-    document.querySelector('input'.value = '');
+
+
+    amigos.push(nome);
+
+
+    atualizarLista();
+
+
+    input.value = '';
 }
 
+
+function atualizarLista() {
+    const lista = document.getElementById('listaAmigos');
+
+    
+    lista.innerHTML = '';
+
+    
+    amigos.forEach((amigo) => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+
+function sortearAmigo() {
+
+    if (amigos.length === 0) {
+        alert('Nenhum amigo na lista para sortear.');
+        return;
+    }
+
+ 
+    const indiceSorteado = Math.floor(Math.random() * amigos.length);
+
+
+    const amigoSorteado = amigos[indiceSorteado];
+
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<li>O amigo sorteado é: <strong>${amigoSorteado}</strong></li>`;
+}
